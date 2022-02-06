4712eda509ed77d8f693f23c6eda5fa57344ccc2
PriyankaVagadre <priyankavagadre@hotmail.com>
2022-02-06 16:08:55 +0530

    event handlers

--- my-app/src/components/Expenses/ExpenseItem.js
+++ my-app/src/components/Expenses/ExpenseItem.js
@@ -1,9 +1,13 @@
 import './ExpenseItem.css';
 function ExpenseItem(props){
+    function clickHandler(){
+        console.log("Clicked !!")
+    }
     return <div className='expenses'>
         <div>{props.title}</div>
         <div>{props.ammount}</div>
         <div>{props.date}</div>
+        <button onClick={clickHandler}>Click Events</button>
     </div>
 }
 export default ExpenseItem;
