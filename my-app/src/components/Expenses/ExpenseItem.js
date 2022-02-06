503ba6a56a6ee74839d8b3effeaf8b02b1799c74
PriyankaVagadre <priyankavagadre@hotmail.com>
2022-02-06 16:30:31 +0530

    useState for react state management

--- my-app/src/components/Expenses/ExpenseItem.js
+++ my-app/src/components/Expenses/ExpenseItem.js
@@ -1,12 +1,20 @@
 import './ExpenseItem.css';
+import React, {useReducer, useState} from 'react';
 function ExpenseItem(props){
+   const [ammount, newAmmount] = useState(props.ammount);
+   const [title, changeTitle] = useState(props.title);
+   const [date, changeDate] = useState(props.date)
+   
     function clickHandler(){
+        newAmmount(ammount+2)
+        changeTitle(title+ 'Changed')
+        changeDate('03/04/20')
         console.log("Clicked !!")
     }
     return <div className='expenses'>
-        <div>{props.title}</div>
-        <div>{props.ammount}</div>
-        <div>{props.date}</div>
+        <div>{title}</div>
+        <div>{ammount}</div>
+        <div>{date}</div>
         <button onClick={clickHandler}>Click Events</button>
     </div>
 }
