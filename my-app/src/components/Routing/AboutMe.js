0652793c5217cd33256d8f84706ba761ad265eef
PriyankaVagadre <priyankavagadre@hotmail.com>
2022-04-07 21:36:19 +0530

    useReducer Hook - when you have to manage multple states

--- my-app/src/components/Routing/AboutMe.js
+++ my-app/src/components/Routing/AboutMe.js
@@ -1,16 +1,38 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
+
+const reducer = (state, action)=>{
+  switch(action.type){
+      case "increment":
+        return{count : state.count +1, showText : state.showText}
+      case "showText":
+        return {count:state.count, showText :!state.showText}
+      default:
+        return state
+  }
+}
 
 function AboutMePage(){
-    const [count, setCount]= useState(0)
+  //  const [count, setCount]= useState(0)
 
-    function incrementCount(){
-        setCount(count+1)
-    }
+//   function incrementCount(){
+//     setCount(count+1)
+// }
+
+  const [state, dispatch] = useReducer(reducer, ({count: 0, showText: true}))
+
     return<div>About Me Page
         <div>
             <h2>React looks</h2>
-            <h3>{count}</h3>
-            <button onClick={incrementCount}>Increment</button>
+            {/* <h3>{count}</h3> */}
+            {/* <button onClick={incrementCount}>Increment</button> */}
+
+            <h3>{state.count}</h3>
+            <button onClick={() =>{
+                dispatch({type: "increment"});
+                dispatch({type: "showText"})
+            }
+        }>Increment and Change Text</button> 
+            {state.showText && <p>Show Text</p>}
         </div>
     </div>
     }
